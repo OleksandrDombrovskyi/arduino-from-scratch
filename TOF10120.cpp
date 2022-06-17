7a9c7fd00af8eefe842ba07b909231de2217f4fc
Dombrovskyi <doleksandr@playtika.com>
2022-06-17 15:18:07 +0300

    Added errors handling, set timeout and Wire reset on timeout. Seems the problem with hanging SOLVED

--- TOF10120.cpp
+++ TOF10120.cpp
@@ -22,6 +22,7 @@ unsigned char dirsend_flag=0;
 
 void TOF10120::initSensor() {
   Wire.begin();
+  Wire.setWireTimeout(100000, true); //true - reset Wire upon timeout
 }
 
 //call the method in timer subroutine
@@ -52,6 +53,12 @@ int TOF10120::ReadDistance(){
     lenth_val=i2c_rx_buf[0];
     lenth_val=lenth_val<<8;
     lenth_val|=i2c_rx_buf[1];
+    if (distance == lenth_val) {
+      //increment timeout
+      Serial.print("Distance was not changed: ");
+      Serial.print(distance);
+      Serial.println(" mm");
+    }
     return lenth_val;
 }
 
@@ -62,13 +69,26 @@ void TOF10120::SensorRead(unsigned char addr,unsigned char* datbuf,unsigned int 
   // the address specified in the datasheet is 164 (0xa4)
   // but i2c adressing uses the high 7 bits so it's 82
   Wire.write(byte(addr));      // sets distance data address (addr)
-  Wire.endTransmission();      // stop transmitting
+  byte error = Wire.endTransmission();      // stop transmitting
+  if (error) {
+    Serial.print("Error #");
+    switch(error) {
+      case 1: Serial.println("1: data too long to fit in transmit buffer."); break;
+      case 2: Serial.println("2: received NACK on transmit of address."); break;
+      case 3: Serial.println("3: received NACK on transmit of data."); break;
+      case 4: Serial.println("4: other error."); break;
+      case 5: Serial.println("5: timeout."); break;
+      default: Serial.print(error); Serial.println(": unknown.");
+    }
+    return;
+  }
   // step 2: wait for readings to happen
   delay(1);                   // datasheet suggests at least 30uS
   // step 3: request reading from sensor
-  Wire.requestFrom(82, cnt);    // request cnt bytes from slave device #82 (0x52)
+  byte byteNumber = Wire.requestFrom(82, cnt);    // request cnt bytes from slave device #82 (0x52)
   // step 5: receive reading from sensor
-  if (cnt <= Wire.available()) { // if two bytes were received
+  int byteNumAvail = Wire.available();
+  if (cnt <= byteNumAvail) { // if two bytes were received
     *datbuf++ = Wire.read();  // receive high byte (overwrites previous reading)
     *datbuf++ = Wire.read(); // receive low byte as lower 8 bits
   }
